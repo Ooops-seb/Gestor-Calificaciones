f82ee00639bb88bf3a9946b756c230aaff6c5063
Erik Villarreal <dev_seb_3745@proton.me>
2023-07-07 01:33:53 -0500

    Add project files.

---
+++ escolastico/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace escolastico.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
