4a68dedc397c1551cd3eb046969a9e44ff1b5fb7
Erik Villarreal <dev_seb_3745@proton.me>
2023-07-08 01:10:45 -0500

    Singin/Up/Apply Controllers added

---
+++ escolastico/Controllers/SingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace escolastico.Controllers
+{
+    public class SingController : Controller
+    {
+        private readonly ILogger<SingController> _logger;
+
+        public SingController(ILogger<SingController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult In()
+        {
+            return View();
+        }
+        public IActionResult Up()
+        {
+            return View();
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
